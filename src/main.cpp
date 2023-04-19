5a45fd84252356723411fe91cde99543dd813748
Dakhay <matthieu.dipaola@icloud.com>
2023-04-19 18:15:52 +0200

    Test

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <DigiKeyboard.h>
+#include <DigiKeyboardFr.h>
+void setup() {
+}
+
+void loop() {
+
+DigiKeyboard.sendKeyStroke(KEY_SPACE,MOD_GUI_LEFT) ; 
+DigiKeyboardFr.delay(500) ; 
+
+DigiKeyboardFr.print("terminal"); 
+DigiKeyboardFr.delay(500) ; 
+
+DigiKeyboardFr.sendKeyStroke(KEY_ENTER);
+DigiKeyboardFr.delay(500) ;
+
+DigiKeyboardFr.print("echo Hack the Planet"); 
+DigiKeyboardFr.delay(500); 
+
+DigiKeyboardFr.sendKeyStroke(KEY_ENTER);
+DigiKeyboardFr.delay(5000) ;
+}
+
